011039c6a6b73b4e2b248e09df721231d926933a
Taran Mittal <taranmittal09@gmail.com>
2025-09-18 21:22:56 +1000

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,9 +20,9 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/howuse/intro">     // NEEDS PATH UPDATE
+            to="/docs/howuse/intro">     
             Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          </Link>  // needs path change
         </div>
       </div>
     </header>
